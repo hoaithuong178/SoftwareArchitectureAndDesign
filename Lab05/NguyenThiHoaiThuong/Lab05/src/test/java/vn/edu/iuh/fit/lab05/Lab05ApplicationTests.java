eacb6f7600071274ccd4c6423d1b31d74f8da845
Nguyen Thi Hoai Thuong <nguyenthihoaitthuong@gmail.com>
2024-04-27 09:21:56 +0700

    Update lab05
    Lab05

---
+++ Lab05/NguyenThiHoaiThuong/Lab05/src/test/java/vn/edu/iuh/fit/lab05/Lab05ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.lab05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
